be95fcffda79618dae2656af78dafc71aac3e0b1
akshay-varun <akshaygr4@gmail.com>
2020-05-26 11:46:59 +0530

    Done with admin accept.

--- public/accept.php
+++ public/accept.php
@@ -3,9 +3,9 @@ $conn = mysqli_connect('localhost', 'dbms', 'hdjdh83748jfjf#@A', 'tutorial');
 if(isset($_POST['update']))
 {
     $email=$_POST['mail'];
-    $sql="UPDATE form set status=1";
+    $sql="update form set status=1 where email='$email'";
     if (mysqli_query($conn, $sql)) {
-
+        header('location: admin_dashboard.php');
     } else {
         echo "Error: " . $sql . "
 " . mysqli_error($conn);
