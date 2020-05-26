57587aa2f764e9596a911f3f698aa14f35fd66d8
akshay-varun <akshaygr4@gmail.com>
2020-05-26 12:34:52 +0530

    Done with user dashboard.

--- public/index.php
+++ public/index.php
@@ -12,20 +12,15 @@ if (isset($_GET['logout'])) {
     header("location: login.php");
 }
 
-echo $_SESSION['username'];
+
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Home</title>
+    <title>User Dashboard</title>
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-
-<div class="header">
-    <h2>Please fill the form</h2>
-</div>
-
     <!-- notification message -->
     <?php if (isset($_SESSION['success'])) : ?>
         <div class="error success" >
@@ -40,13 +35,60 @@ echo $_SESSION['username'];
 
     <!-- logged in user information -->
     <?php  if (isset($_SESSION['username'])) : ?>
-        <p>Welcome <?php echo $_SESSION['username']; ?></p>
+        <p class="header">Welcome <?php echo $_SESSION['username']; ?></p>
 
 
-        <p> <a href="index.php?logout='1'" style="color: red;">logout</a> </p>
+        <p> <a href="index.php?logout='1'" style="color: red;" class="logout">LOGOUT</a> </p>
     <?php endif ?>
 
-<a href="fill-form.php">Fill the form</a>
+
+    <?php
+    if (!isset($_SESSION['username'])) {
+        $_SESSION['msg'] = "You must log in first";
+    }
+    $conn = mysqli_connect('localhost', 'dbms', 'hdjdh83748jfjf#@A', 'tutorial');
+    $status=0;
+    $user= '';
+       $user=$_SESSION['username'];
+    $sql = "select first_name from form where email in(select email from users where username='$user');";
+    $result=$conn->query($sql);
+    if ($result) {
+// output data of each row
+        while ($row = $result->fetch_assoc()) {
+            $status= $row["first_name"] . "";
+        }
+    }
+    $conn->close();
+    if($status) {
+        echo "
+        <br>
+        <br>
+            <h1 style='text-align: center;color: brown ' > You have already filled the form </h1>
+        <br>
+        <br>
+            <h2 style='text-align: center;color: blueviolet'>Check you Application Status</h2>
+           
+           <br>
+           <br>
+            <form action=\"status.php\">
+              <input type=\"submit\" value=\"Check Status\" class='button'>
+            </form>";
+
+    }
+    else
+    {
+        echo "
+              <br>
+              <br> 
+        <h1 style='text-align: center;color: brown '>Kindly Fill the form here </h1>
+         <br>
+          <br>
+          <form action=\"fill-form.php\">
+        <input type=\"submit\" value=\"Fill the Form\" class='button'>
+        </form> 
+        ";
+    }
+    ?>
 
 </body>
 </html>
